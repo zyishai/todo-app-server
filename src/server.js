d38fe2e6d9740221b1ea443e83ace08a02c2dbf9
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-16 20:11:31 +0300

    simpler setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
-const jsonServer = require('json-server');
-const path = require('path');
-const router = jsonServer.router(path.resolve(__dirname, '..', 'db.json'));
+// const jsonServer = require('json-server');
+// const path = require('path');
+const router = require('express').Router();
+const bodyParser = require('body-parser');
+// const router = jsonServer.router(path.resolve(__dirname, '..', 'db.json'));
+
+router.use(bodyParser.json());
+router.get('/', (_, res) => {
+    res.json({
+        status: 'OK',
+        message: 'Hello world!'
+    });
+});
 
 module.exports = router;
