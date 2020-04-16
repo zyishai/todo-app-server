a467f84df0f8c911346afbc3ed424f1c4ff444ab
Yishai Zehavi <zehaviyishai@gmail.com>
2020-04-16 18:56:00 +0300

    dangerous try

--- src/api.js
+++ src/api.js
@@ -1,25 +1,14 @@
 const serverless = require('serverless-http');
-// const jsonServer = require('json-server');
+const jsonServer = require('json-server');
 const path = require('path');
-// const router = jsonServer.router(path.resolve(__dirname, '..', 'db.json'));
-// const middlewares = jsonServer.defaults({
-//     static: path.resolve(__dirname, '../dist')
-// });
+const router = jsonServer.router(path.resolve(__dirname, '..', 'db.json'));
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-
-// const server = jsonServer.create();
-
-// server.use(middlewares);
 const express = require('express');
 const server = express();
-const router = express.Router();
-router.get('/', (req, res) => {
-    res.json({
-        status: 'OK',
-        message: 'Hello, world!'
-    });
-});
+server.use(express.static(path.resolve(__dirname, '../public')));
+router.use(express.static(path.resolve(__dirname, '../public')));
 server.use('/.netlify/functions/api', router);
 
 server.listen(port);
